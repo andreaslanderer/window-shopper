76278e712a5c413fede7fb7b132140514868b6ed
Andreas Landerer <andreas.landerericloud.com>
2017-11-25 19:32:03 +0100

    Second version of the app: added second custom text field and a keyboard with a custom input accessory view

--- window-shopper/Controller/MainViewController.swift
+++ window-shopper/Controller/MainViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculateButton = UIButton(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: self.view.frame.size.width,
+            height: 60))
+        calculateButton.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButton.addTarget(
+            self,
+            action: #selector(MainViewController.calculate),
+            for: .touchUpInside)
+        
+        self.wageTextField.inputAccessoryView = calculateButton
+        self.priceTextField.inputAccessoryView = calculateButton
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func calculate() {
+        print("Called calculate...")
     }
-
-
 }
 
--- window-shopper/View/CurrencyTextField.swift
+++ window-shopper/View/CurrencyTextField.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
 
     override func awakeFromNib() {
+        self.customizeView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        self.customizeView()
+    }
+    
+    func customizeView() {
         super.awakeFromNib()
         self.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25) // Color: #FFF, Alpha: 0.25
         self.layer.cornerRadius = 5.0
         self.textAlignment = .center
         self.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.keyboardType = .decimalPad
+        self.keyboardAppearance = .dark
+        self.enablesReturnKeyAutomatically = true
         self.setAttributedPlaceholder()
     }
     
